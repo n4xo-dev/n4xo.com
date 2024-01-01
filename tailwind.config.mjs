c572cec3425641a5842e0ef8b3ef770dad1e19e4
Ignacio Lopezosa Serrano <ilopezosaserrano@gmail.com>
2024-01-01 20:46:12 +0100

    Landing welcome

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+import defultTheme from 'tailwindcss/defaultTheme';
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'n4xo-light': '#F0EDE7',
+				'n4xo-beige': '#A98D85',
+				'n4xo-brown': '#5E4C4A',
+				'n4xo-dark': '#282828',
+			},
+			fontFamily: {
+				sans: ['JetBrains Mono', ...defultTheme.fontFamily.sans],
+			},
+			fontSize: {
+				...defultTheme.fontSize,
+				'6.5xl': '4rem',
+			}
+		},
 	},
 	plugins: [],
 }
