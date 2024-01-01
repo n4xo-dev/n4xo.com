3bcdafcc22c448d13cb8e58dac049c273e1d04dc
Ignacio Lopezosa Serrano <ilopezosaserrano@gmail.com>
2024-01-01 23:19:23 +0100

    dark mode switch

---
+++ src/components/ThemeSwitch.svelte
@@ -0,0 +1,49 @@
+<script>
+  import { onMount } from 'svelte';
+  let isDark = false;
+
+  function updateTheme() {
+    document.documentElement.classList.toggle('dark', isDark);
+    localStorage.setItem('theme', isDark ? 'dark' : 'light');
+  }
+
+  onMount(() => {
+    const savedTheme = localStorage.getItem('theme');
+    isDark = savedTheme === 'dark';
+    updateTheme();
+  })
+
+  function toggleTheme() {
+    isDark = !isDark;
+    updateTheme();
+  }
+</script>
+
+<div class="toggle-container {isDark ? 'on' : ''}" on:click={toggleTheme}>
+  <div class="toggle-switch"></div>
+</div>
+
+<style>
+  .toggle-container {
+      width: 60px;
+      height: 30px;
+      background-color: #A98D85;
+      border-radius: 15px;
+      position: relative;
+  }
+
+  .toggle-switch {
+      width: 24px;
+      height: 24px;
+      background-color: white;
+      position: absolute;
+      top: 3px;
+      left: 3px;
+      border-radius: 50%;
+      transition: 0.3s;
+  }
+
+  .toggle-container.on .toggle-switch {
+      left: 33px;
+  }
+</style>
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,7 @@ import defultTheme from 'tailwindcss/defaultTheme';
 
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
 	theme: {
 		extend: {
 			colors: {
