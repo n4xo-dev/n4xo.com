be217f1f403e4c0e7cf3c02703659ce58784090a
Ignacio Lopezosa Serrano <ilopezosaserrano@gmail.com>
2024-01-02 22:30:05 +0100

    Responsive hamburger menu WORKING

--- src/components/HamburgerMenu.svelte
+++ src/components/HamburgerMenu.svelte
@@ -1,4 +1,5 @@
 <script>
+  import ThemeSwitch from './themeSwitch.svelte';
   let isOpen = false;
 </script>
 
@@ -11,20 +12,16 @@
   </button>
 
   {#if isOpen}
-    <div class="absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+    <div class="absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-white dark:bg-stone-900 ring-1 ring-black ring-opacity-5">
       <div class="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
         <!-- Navigation links and theme switch here -->
         <nav class="flex flex-col gap-4 p-4">
           <a href="#about" class="font-medium">About</a>
-          <u class="no-underline text-n4xo-brown dark:text-n4xo-beige font-extrabold">&gt;&gt;</u>
           <a href="#projects" class="font-medium">Projects</a>
-          <u class="no-underline text-n4xo-brown dark:text-n4xo-beige font-extrabold">&gt;&gt;</u>
           <a href="#resume" class="font-medium">Resume</a>
-          <u class="no-underline text-n4xo-brown dark:text-n4xo-beige font-extrabold">&gt;&gt;</u>
           <a href="#blog" class="font-medium">Blog</a>
-          <u class="no-underline text-n4xo-brown dark:text-n4xo-beige font-extrabold">&gt;&gt;</u>
           <a href="#contact" class="font-medium">Contact</a>
-          <ThemeSwitch client:load/>
+          <ThemeSwitch />
         </nav>
       </div>
     </div>
