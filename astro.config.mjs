d00c499b9c7659471813adda04a5f4519d0c4bec
Ignacio Lopezosa Serrano <ilopezosaserrano@gmail.com>
2024-01-04 19:44:51 +0100

    Added Vercel Analytics PROPERLY

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from 'astro/config';
+import vercel from "@astrojs/vercel/static";
 import tailwind from "@astrojs/tailwind";
+
 
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()]
+  integrations: [tailwind(), svelte()],
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
